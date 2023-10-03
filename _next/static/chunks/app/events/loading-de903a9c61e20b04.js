b42b6e2b6fddf7304747cf17d2bf802ae4e7def3
maheshgracia <mahesh@graciamarcom.com>
2023-10-03 11:24:39 +0530

    first commit

---
+++ _next/static/chunks/app/events/loading-de903a9c61e20b04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[728,160,555,450,554,990,929,38],{3521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);

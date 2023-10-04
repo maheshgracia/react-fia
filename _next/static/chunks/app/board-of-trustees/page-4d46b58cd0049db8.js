196f5bf37deec5abfaef929af582fa95672ac125
maheshgracia <mahesh@graciamarcom.com>
2023-10-04 18:15:10 +0530

    new remote

--- _next/static/chunks/app/board-of-trustees/page-4d46b58cd0049db8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[929,160,555,450,554,990,38,728],{3521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- _next/static/chunks/app/loading-8f117009f13d04c9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[555,160,450,554,990,929,38,728],{3521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);

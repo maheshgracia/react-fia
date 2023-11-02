2a9e200e69bd5fb6a6ac3f29d1775916794556a8
maheshgracia <mahesh@graciamarcom.com>
2023-11-02 17:13:12 +0530

    banner size

---
+++ _next/static/chunks/app/executive-committee/loading-f066cf3cf2d47eef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[792,555,854,633,40,218,554,990,728,652,192,533,450,23,147,929,912,113,144,575,878],{3521:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);

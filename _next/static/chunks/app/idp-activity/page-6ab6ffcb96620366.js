f9e2e12bd13b535cd4881304a116c5cd5b15056f
maheshgracia <mahesh@graciamarcom.com>
2024-02-01 12:44:35 +0530

    Mr.BOT

---
+++ _next/static/chunks/app/idp-activity/page-6ab6ffcb96620366.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2098,9160,3499,1963,6376,38,8566,5585,3400,533,2016,6370,3495],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

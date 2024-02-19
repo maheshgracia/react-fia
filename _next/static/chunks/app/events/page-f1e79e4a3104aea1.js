0e4aaa5619e308e47eae9d24b4965e7e49c4957d
maheshgracia <mahesh@graciamarcom.com>
2024-02-19 16:55:46 +0530

    iwd-event

---
+++ _next/static/chunks/app/events/page-f1e79e4a3104aea1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[38,9160,3499,8566,6376,3400,5585,533,2098,2016,3495,1963,6370],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

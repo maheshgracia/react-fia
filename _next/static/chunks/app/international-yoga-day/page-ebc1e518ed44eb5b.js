2759fc47783a9a47aca25ba957e4b00a7e71cb93
maheshgracia <mahesh@graciamarcom.com>
2024-05-06 12:02:37 +0530

    NL-114

---
+++ _next/static/chunks/app/international-yoga-day/page-ebc1e518ed44eb5b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2016,9160,1963,3499,38,8566,6376,1780,3400,2098,5585,3495,2159,6370,533],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

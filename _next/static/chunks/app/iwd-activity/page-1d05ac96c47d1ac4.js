a1ff923ae0788c54fd5ed70b8e5ab0dbdab440b1
maheshgracia <mahesh@graciamarcom.com>
2024-08-06 13:18:42 +0530

    socialUpdate

---
+++ _next/static/chunks/app/iwd-activity/page-1d05ac96c47d1ac4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2159,9160,1963,38,3499,5585,1780,3400,2098,533,2016,6376,6370,3495,8566],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

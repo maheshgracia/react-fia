d0fa65742182aeaf4c15b8bba5c7b052f034677e
maheshgracia <mahesh@graciamarcom.com>
2024-08-09 17:09:45 +0530

    accordian

---
+++ _next/static/chunks/app/gala/page-0e71df522abf46e6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1780,9160,1963,3499,38,8566,6376,5585,3400,2016,533,2159,2098,6370,3495],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

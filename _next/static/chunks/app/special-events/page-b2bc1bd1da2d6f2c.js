5bb53024857b4682379a9520f37e2d2fbe5a6f24
maheshgracia <mahesh@graciamarcom.com>
2024-12-11 16:21:14 +0530

    responsive

---
+++ _next/static/chunks/app/special-events/page-b2bc1bd1da2d6f2c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6370,9160,1963,3499,38,6376,5585,8566,3400,1780,533,2159,2016,3495,2098],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

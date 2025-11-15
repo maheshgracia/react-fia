995cf7cb30b73c2be0a13fd6945efbb1e7c93294
maheshgracia <mahesh@graciamarcom.com>
2025-11-15 11:54:04 +0530

    newsportal

---
+++ _next/static/chunks/app/admin/news/page-f257d0e0c6d51be8.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9229],{2230:function(e,n,t){Promise.resolve().then(t.bind(t,782))},782:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return AdminNews}});var r=t(7437),i=t(2265);function AdminNews(){let[e,n]=(0,i.useState)([]);(0,i.useEffect)(()=>{fetch("/api/news").then(e=>e.json()).then(n)},[]);let remove=async t=>{await fetch("/api/news",{method:"DELETE",headers:{"Content-Type":"application/json"},body:JSON.stringify({id:t})}),n(e.filter(e=>e._id!==t))};return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Manage News"}),(0,r.jsx)("a",{href:"/admin/news/add",children:"Add News"}),e.map(e=>(0,r.jsxs)("div",{children:[(0,r.jsx)("img",{src:e.thumbnail,width:"60",alt:"img"}),e.title,(0,r.jsx)("button",{onClick:()=>remove(e._id),children:"Delete"})]},e._id))]})}},622:function(e,n,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var r=t(2265),i=Symbol.for("react.element"),s=Symbol.for("react.fragment"),o=Object.prototype.hasOwnProperty,a=r.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,n,t){var r,s={},f=null,u=null;for(r in void 0!==t&&(f=""+t),void 0!==n.key&&(f=""+n.key),void 0!==n.ref&&(u=n.ref),n)o.call(n,r)&&!c.hasOwnProperty(r)&&(s[r]=n[r]);if(e&&e.defaultProps)for(r in n=e.defaultProps)void 0===s[r]&&(s[r]=n[r]);return{$$typeof:i,type:e,key:f,ref:u,props:s,_owner:a.current}}n.Fragment=s,n.jsx=q,n.jsxs=q},7437:function(e,n,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[2971,7864,1744],function(){return e(e.s=2230)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/parade-map/layout-02b7ce2a624c4f11.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7631],{1868:function(e,r,t){Promise.resolve().then(t.bind(t,4119))},4119:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return ParadeMapLayout}});var n=t(7437),o=t(2265);function ParadeMapLayout(e){let{children:r}=e;return(0,o.useEffect)(()=>(document.body.classList.add("parade-map-page"),()=>{document.body.classList.remove("parade-map-page")}),[]),(0,n.jsx)(n.Fragment,{children:r})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),a=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,a={},c=null,i=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(i=r.ref),r)u.call(r,n)&&!f.hasOwnProperty(n)&&(a[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===a[n]&&(a[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:i,props:a,_owner:s.current}}r.Fragment=a,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[2971,7864,1744],function(){return e(e.s=1868)}),_N_E=e.O()}]);

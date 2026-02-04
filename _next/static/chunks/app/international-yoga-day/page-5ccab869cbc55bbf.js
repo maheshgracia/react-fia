5daf08894dc366e4fa5b4ba8ee53781509cfeebc
maheshgracia <mahesh@graciamarcom.com>
2026-02-04 14:50:34 +0530

    dpc flayer

---
+++ _next/static/chunks/app/international-yoga-day/page-5ccab869cbc55bbf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2016,9160,1963,3499,4852,1648,3770,198,38,5585,6376,8566,2092,3400,533,4116,2159,1780,3495,7747,6370,9835],{2807:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[4724,2971,7864,1744],function(){return n(n.s=2807)}),_N_E=n.O()}]);

5ab114296501f200f110172197935e31e0d8a15d
maheshgracia <146514917+maheshgracia@users.noreply.github.com>
2026-05-20 16:59:06 +0530

    idp-changes

---
+++ _next/static/chunks/app/gala/page-32b2840d012ce000.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1780,9160,1963,3770,1648,4852,198,38,6376,8566,5585,3499,3400,533,2016,4116,2159,2092,3495,7747,6370,9835],{1714:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[8326,2971,2472,1744],function(){return n(n.s=1714)}),_N_E=n.O()}]);

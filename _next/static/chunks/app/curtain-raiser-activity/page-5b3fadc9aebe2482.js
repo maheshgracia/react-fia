1acd983511e824577775e34ab4a3c75810c6ee96
mahesh <mahesh@graciamarcom.com>
2026-06-26 11:21:17 +0530

    NL177

---
+++ _next/static/chunks/app/curtain-raiser-activity/page-5b3fadc9aebe2482.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4852,9160,3770,1648,1963,3499,198,38,6376,5585,8566,3400,1780,2092,2016,533,4116,2159,3495,7747,6370,9835],{7170:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[8326,2971,2472,1744],function(){return n(n.s=7170)}),_N_E=n.O()}]);
